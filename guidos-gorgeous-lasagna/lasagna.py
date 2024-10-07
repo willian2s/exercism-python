b101b9de7446d4dd028d390201ea4bea33047f0c
Willian Silva <silva.williansantos@gmail.com>
2024-10-07 14:36:23 -0300

    remove todo comments

--- guidos-gorgeous-lasagna/lasagna.py
+++ guidos-gorgeous-lasagna/lasagna.py
@@ -25,8 +25,6 @@ def bake_time_remaining(elapsed_bake_time: int):
     return EXPECTED_BAKE_TIME - elapsed_bake_time
 
 
-# TODO: Define the 'preparation_time_in_minutes()' function below.
-# You might also consider using 'PREPARATION_TIME' here, if you have it defined.
 def preparation_time_in_minutes(number_of_layers: int):
     """Calculate the preparation time in minutes.
 
@@ -39,8 +37,6 @@ def preparation_time_in_minutes(number_of_layers: int):
     return number_of_layers * PREPARATION_TIME
 
 
-# TODO: define the 'elapsed_time_in_minutes()' function below.
-# Remember to add a docstring (you can copy and then alter the one from bake_time_remaining.)
 def elapsed_time_in_minutes(number_of_layers: int, elapsed_bake_time: int):
     """Calculate the total elapsed time in minutes.
 
