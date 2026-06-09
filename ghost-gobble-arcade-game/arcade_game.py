2aad1b440ec9d07dd5f67ce06b62f3337e4ff1ac
Willian Silva <silva.williansantos@gmail.com>
2026-06-09 11:42:00 -0300

    Complete ghost-gobble-arcade-game exercise implementation

--- ghost-gobble-arcade-game/arcade_game.py
+++ ghost-gobble-arcade-game/arcade_game.py
@@ -13,7 +13,7 @@ def eat_ghost(power_pellet_active, touching_ghost):
 
     """
 
-    pass
+    return power_pellet_active and touching_ghost
 
 
 def score(touching_power_pellet, touching_dot):
@@ -28,7 +28,7 @@ def score(touching_power_pellet, touching_dot):
 
     """
 
-    pass
+    return touching_power_pellet or touching_dot
 
 
 def lose(power_pellet_active, touching_ghost):
@@ -42,7 +42,7 @@ def lose(power_pellet_active, touching_ghost):
         bool: Has the player lost the game?
     """
 
-    pass
+    return not power_pellet_active and touching_ghost
 
 
 def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
@@ -57,4 +57,4 @@ def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
         bool: Has the player won the game?
     """
 
-    pass
+    return has_eaten_all_dots and (power_pellet_active or not touching_ghost)
